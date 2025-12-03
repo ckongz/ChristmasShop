9b30fe7df4714782da05a18c822fc393970eb2a0
ckongz <139482669+ckongz@users.noreply.github.com>
2025-12-03 14:56:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 
 <?php
 $username = "Customer"; //1
-$greeting = "Hello , ". $username. " ! Welcome to Christmas Cookies Shop!"; //2
+$greeting = "Hello, " . $username . "! Welcome to Christmas Cookies Shop!"; //2
+
 $offer = array(
     "item" => "Christmas Cookies",
     "quantity" => 3,
     "normal_price" => 5,
     "discounted_price" => 4
 ); 
-$usual_price = $offer["quantity"] * $offer["discounted_price"]; 
+
+$usual_price = $offer["quantity"] * $offer["normal_price"]; 
 $offer_price = $offer["quantity"] * $offer["discounted_price"];
 $saving = $usual_price - $offer_price; 
 ?>
@@ -17,22 +19,28 @@ $saving = $usual_price - $offer_price;
 <section class="savings">
     Save $<?php echo $saving; ?>
 </section>
-    <h1><?php echo $greeting; ?></h1>  
-    <?php echo $saving; ?>  
-    <h2>Special Christmas Offer! </h2>
-    <p>Buy <?php echo $offer["quantity"]; ?> packs of <?php echo $offer["item"]; ?> and get amazing discount!</p> <!-- 9 !-->
-    <h3>Normal Price</h3>
-    <p>$<?php echo $usual_price; ?></p>
-    <h3>Special Price</h3>
-    <p>$<?php echo $offer_price; ?></p> 
-    <h2>About Our Christmas Cookies:</h2>
-    <ul>
-        <li>Santa-shaped sugar cookies</li>
-        <li>Christmas tree gingerbread</li>
-        <li>Snowman shortbread</li>
-        <li>Star-shaped cinnamon cookies</li>
-    </ul>
-    <img src="img/cookies-1.jpg" alt="Christmas Cookies">
+
+<h1><?php echo $greeting; ?></h1>  
+
+<h2>Special Christmas Offer!</h2>
+<p>
+    Buy <?php echo $offer["quantity"]; ?> packs of <?php echo $offer["item"]; ?> and get an amazing discount!
+</p>
+
+<h3>Normal Price</h3>
+<p>$<?php echo $usual_price; ?></p>
+
+<h3>Special Price</h3>
+<p>$<?php echo $offer_price; ?></p> 
+
+<h2>About Our Christmas Cookies:</h2>
+<ul>
+    <li>Santa-shaped sugar cookies</li>
+    <li>Christmas tree gingerbread</li>
+    <li>Snowman shortbread</li>
+    <li>Star-shaped cinnamon cookies</li>
+</ul>
+
+<img src="img/cookies-1.jpg" alt="Christmas Cookies">
 
 <?php include 'includes/footer.php'; ?>
-
